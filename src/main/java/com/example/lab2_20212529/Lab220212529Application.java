b0de322323402585f41351f0b9c7f5a51021c900
JJOB73 <a20212529@pucp.edu.pe>
2025-04-11 20:28:18 -0500

    primer commit

---
+++ src/main/java/com/example/lab2_20212529/Lab220212529Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20212529;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab220212529Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab220212529Application.class, args);
+    }
+
+}
